f84bce4a53c4c43f6a9cb0d2fb4819e54ca76d9f
Mattia Evangelisti <71966068+MadMatti@users.noreply.github.com>
2023-11-16 15:51:50 +0100

    added rules association

---
+++ Homework_02/src/association.py
@@ -0,0 +1,37 @@
+from collections import defaultdict, Counter
+from typing import List, Set, Dict, FrozenSet, Tuple
+from itertools import combinations
+from tqdm import tqdm
+
+class AprioriAssociation:
+
+    def __init__(self, transactions: List[Set[int]], frequent_itemsets: Dict[FrozenSet[int], int], min_support: int, min_confidence: float):
+        self.transactions = transactions
+        self.frequent_itemsets = frequent_itemsets
+        self.min_support = min_support
+        self.min_confidence = min_confidence
+
+    def calculate_support(self, itemset: FrozenSet[int]) -> float:
+        return sum(1 for transaction in self.transactions if itemset.issubset(transaction))
+
+    def generate_rules(self) -> List[Tuple[FrozenSet[int], FrozenSet[int], float, float]]:
+        ass_rules = []
+
+        for itemset in tqdm(self.frequent_itemsets):
+            for i in range(1, len(itemset)):
+                for antecedent in combinations(itemset, i):
+                    antecedent = frozenset(antecedent)
+                    consequent = itemset - antecedent
+
+                    rule_support = self.calculate_support(itemset)
+                    # if antecedent in self.frequent_itemsets:
+                    antecedent_support = self.calculate_support(antecedent)
+                    confidence = rule_support / antecedent_support if antecedent_support > 0 else 0
+                    if rule_support >= self.min_support and confidence >= self.min_confidence:
+                            ass_rules.append((antecedent, consequent, rule_support, confidence))
+
+        return ass_rules
+
+
+
+    
--- Homework_02/src/main.py
+++ Homework_02/src/main.py
@@ -1,5 +1,9 @@
 import os
 from apriori import AprioriAlgorithm
+from association import AprioriAssociation
+
+def format_itemset(itemset):
+    return ', '.join(map(str, sorted(itemset)))
 
 def main():
     path = os.path.dirname(os.getcwd())
@@ -11,26 +15,51 @@ def main():
     frequent_singletons = apiroi.find_frequent_singletons(s=min_support)
     print(f"Frequent Singletons: {len(frequent_singletons)}")
     # Print the first 10 frequent singletons
-    print({k: frequent_singletons[k] for k in list(frequent_singletons)[:10]})
+    print({format_itemset(k): frequent_singletons[k] for k in list(frequent_singletons)[:10]})
+
 
     candidate_item_sets = apiroi.generate_candidate_item_sets(frequent_singletons.keys(), 2)
     print(f"Candidate Item Sets: {len(candidate_item_sets)}")
-    print(list(candidate_item_sets)[:10])
+    print([format_itemset(itemset) for itemset in candidate_item_sets][:10])
 
 
     filtered_frequent_item_sets = apiroi.filter_frequent_item_sets(candidate_item_sets, 2, s=min_support)
     print(f"Frequent Item Sets: {len(filtered_frequent_item_sets)}")
-    for item_set, support in list(filtered_frequent_item_sets.items())[:10]:
+    for item_set, support in [(format_itemset(iter[0]), iter[1]) for iter in filtered_frequent_item_sets.items()][:10]: 
         print(f"Item Set: {item_set}, Support: {support}")
+
 
     for i in range(3, max_item_set_length + 1):
         candidate_item_sets = apiroi.generate_candidate_item_sets(filtered_frequent_item_sets.keys(), i)
-        print(list(candidate_item_sets)[:10])
+        print([format_itemset(itemset) for itemset in candidate_item_sets][:10])
 
         filtered_frequent_item_sets = apiroi.filter_frequent_item_sets(candidate_item_sets, i, s=min_support)
         print(f"Frequent Item Sets of length {i}: {len(filtered_frequent_item_sets)}")
-        for item_set, support in list(filtered_frequent_item_sets.items())[:10]:
+        for item_set, support in [(format_itemset(iter[0]), iter[1]) for iter in filtered_frequent_item_sets.items()][:10]:
             print(f"Item Set: {item_set}, Support: {support}")
 
+
+def main_rules():
+    path = os.path.dirname(os.getcwd())
+    data_path = os.path.join(path, 'data', 'T10I4D100K.dat')
+    apiroi = AprioriAlgorithm(data_path)
+    min_support = 100
+    transactions = apiroi.read_dataset(data_path)
+
+    frequent_singletons = apiroi.find_frequent_singletons(s=min_support)
+    candidate_item_sets = apiroi.generate_candidate_item_sets(frequent_singletons.keys(), 2)
+    filtered_frequent_item_sets = apiroi.filter_frequent_item_sets(candidate_item_sets, 2, s=min_support)
+
+    rules_generator = AprioriAssociation(transactions, filtered_frequent_item_sets, min_support=100, min_confidence=0.7)
+    rules = rules_generator.generate_rules()
+
+    for rule in rules:
+        antecedent, consequent, support, confidence = rule
+        formatted_antecedent = format_itemset(antecedent)
+        formatted_consequent = format_itemset(consequent)
+        print(f"Rule: {formatted_antecedent} -> {formatted_consequent}, Support: {support}, Confidence: {confidence}")
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    main_rules()
